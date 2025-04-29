1b2ab91855d1341aa35d98e97b8def3d3729188e
Shohaib Mallick <>
2025-04-29 18:59:41 -0400

    feat: Research and Publications Component
    Added Components:
    - research.jsx
    
    Updated Components:
    - page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,18 @@ import ScrollLinked from "./pageProgress.jsx";
 import AboutMe from "./aboutMe.jsx";
 import Education from "./education.jsx";
 import Experience from "./experience.jsx";
+import Research from "./research.jsx";
 
 export default function Home() {
   return (
     <div className="lg:px-8 py-6">
       <ScrollLinked />
       <AboutMe />
-      <div className=" grid grid-cols-1 lg:grid-cols-2">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
         <Experience />
         <Education />
       </div>
+      <Research />
     </div>
   );
 }
