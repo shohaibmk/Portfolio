492d5cf3738446a5402540b736f6664b35d2418f
Shohaib Mallick <51514654+shohaibmk@users.noreply.github.com>
2025-11-20 20:47:39 +0530

    Update landing.jsx

--- app/components/landing.jsx
+++ app/components/landing.jsx
@@ -4,7 +4,7 @@ import { skills } from '../data/skills';
 
 const PROFILE_IMAGE = {
     // src: "https://media.licdn.com/dms/image/v2/D4E03AQHFdYjW4O2XIw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1731003250675?e=1751500800&v=beta&t=RA7of28EQvObskW6rtYMo8VoK7DbNebEbNBLv4M6QU8",
-    src: "https://media.licdn.com/dms/image/v2/D4E03AQHFdYjW4O2XIw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1731003250650?e=1762387200&v=beta&t=eOv5KO6sdCzoF2fuG6624sJ72Rti434V25-z4XsO3Cc",
+    src: "https://media.licdn.com/dms/image/v2/D4E03AQHFdYjW4O2XIw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1731003250650?e=1765411200&v=beta&t=hMHWgUxkbloBwc24f_qLw6rSbpgi-XdNdx_ubKnA-qU",
     alt: "Shohaib Mallick",
     width: 180,
     height: 180
