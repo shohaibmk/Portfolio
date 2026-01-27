a22fb70d9d160798b5519b33976049c6ed22b218
Shohaib Mallick <51514654+shohaibmk@users.noreply.github.com>
2026-01-27 20:19:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     },{
       protocol: 'https',
       hostname: 'res.cloudinary.com',
-      pathname: 'dbqwc4btc/image/upload/**',
+      pathname: '/dbqwc4btc/image/upload/**',
     }],
   },
 };
