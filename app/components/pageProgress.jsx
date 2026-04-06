64769689576b3987f049359d2902e9d38a985991
Shohaib Mallick <shohaibm99@gmail.com>
2026-04-06 22:59:26 +0530

    Fixed formatting

--- app/components/pageProgress.jsx
+++ app/components/pageProgress.jsx
@@ -1,46 +1,46 @@
-"use client"
+"use client";
 
-import Landing from "./landing.jsx"
-import { motion, useSpring, useScroll } from "motion/react"
-import { useEffect } from "react"
-import { initializeAnalytics } from "../firebase/analytics"
+import Landing from "./landing.jsx";
+import { motion, useSpring, useScroll } from "motion/react";
+import { useEffect } from "react";
+import { initializeAnalytics } from "../firebase/analytics";
 
 export default function ScrollLinked() {
-    useEffect(() => {
-        initializeAnalytics();
-    }, []);
+  useEffect(() => {
+    initializeAnalytics();
+  }, []);
 
-    const { scrollYProgress } = useScroll()
-    const scaleX = useSpring(scrollYProgress, {
-        stiffness: 100,
-        damping: 30,
-        restDelta: 0.001,
-    })
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
-    return (
-        <>
-            <motion.div
-                id="scroll-indicator"
-                className="bg-[#ef9c82]"
-                style={{
-                    scaleX,
-                    position: "fixed",
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    height: 10,
-                    originX: 0,
-                }}
-            />
-            <Content />
-        </>
-    )
+  return (
+    <>
+      <motion.div
+        id="scroll-indicator"
+        className="bg-[#ef9c82]"
+        style={{
+          scaleX,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 10,
+          originX: 0,
+        }}
+      />
+      <Content />
+    </>
+  );
 }
 
 function Content() {
-    return (
-        <>
-            <Landing />
-        </>
-    )
+  return (
+    <>
+      <Landing />
+    </>
+  );
 }
