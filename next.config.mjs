f6954fae2dac3616e128980c09e02a14aba89237
Shohaib Mallick <shohaibm99@gmail.com>
2026-05-03 14:25:24 +0530

    Updated Images qualities in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ["192.168.1.8"],
   images: {
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: 'media.licdn.com',
-      pathname: '/dms/image/**',
-    },{
-      protocol: 'https',
-      hostname: 'res.cloudinary.com',
-      pathname: '/dbqwc4btc/image/upload/**',
-    }],
+    qualities: [75, 100],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        pathname: "/dms/image/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dbqwc4btc/image/upload/**",
+      },
+    ],
   },
 };
 
